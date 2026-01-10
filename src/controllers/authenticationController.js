f3aa7409a0708435e01e1e736150c3a361f4c807
Giovani Cavalheri <inocenciogiovanin@gmail.com>
2026-01-10 17:00:38 -0300

    login route created with jwt

--- src/controllers/authenticationController.js
+++ src/controllers/authenticationController.js
@@ -1,5 +1,11 @@
+const jwt = require("jsonwebtoken");
+
+const users = require("../models/users");
+
+const secretKey = "vegeta-is-better-than-Goku";
+
 const authenticationController = {
-    //POST /auth/register
+  //POST /authentication/register
   register: (req, res) => {
     const { name, email, password } = req.body;
 
@@ -12,5 +18,40 @@ const authenticationController = {
     };
 
     users.push(newUser);
+    return res.status(201).json({
+      message: "User created successfully!",
+      user: {
+        id: newUser.id,
+        name: newUser.name,
+        email: newUser.email,
+        role: newUser.role,
+      },
+    });
+  },
+
+  // ==> Login
+  //POST /authentication/login
+  login: (req, res) => {
+    const { email, password } = req.body;
+
+    const findEmail = users.find((user) => user.email === email);
+
+    if (!findEmail) {
+      return res.status(400).json({ message: "Invalid Credentials." });
+    }
+
+    const findPassword = users.find((user) => user.password !== password);
+
+    if (!findPassword) {
+      return res.status(401).json({ message: "Invalid Credentials" });
+    }
+
+    const payload = { id, name, email, role };
+
+    const token = jwt.sign(payload, secretKey, { expiresIn: "1h" });
+
+    res.json({ token });
   },
 };
+
+module.exports = authenticationController;
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const authentication = express.Router();
+
 const secretKey = "vegeta-is-better-than-Goku";
 const jwt = require("jsonwebtoken");
 
 
-authentication.post('/login',)
+authentication.post('/login')
